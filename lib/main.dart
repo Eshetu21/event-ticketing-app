f72c91861c9f25699e6d98e1daae104d30715db0
Eshetu21 <eshetutesema2050@gmail.com>
2025-03-02 17:56:49 +0300

    added auth states

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:crypto/core/theme/theme.dart';
+import 'package:crypto/features/auth/data/datasources/auth_remote_data_source.dart';
+import 'package:crypto/features/auth/data/repositories/auth_repository_imp.dart';
+import 'package:crypto/features/auth/domain/usecases/login_usecase.dart';
+import 'package:crypto/features/auth/domain/usecases/register_usercase.dart';
+import 'package:crypto/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:crypto/pages/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MainApp());
@@ -11,9 +17,22 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        theme: AppTheme.lightThemeMode,
-        debugShowCheckedModeBanner: false,
-        home: SplashScreen());
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+            create: (_) => AuthBloc(
+                registerUsercase: RegisterUsercase(
+                    authRepository: AuthRepositoryImp(
+                        authRemoteDataSource: AuthRemoteDataSource())),
+                loginUsecase: LoginUsecase(
+                    authRepository: AuthRepositoryImp(
+                        authRemoteDataSource: AuthRemoteDataSource()))))
+      ],
+      child: MaterialApp(
+          theme: AppTheme.lightThemeMode,
+          debugShowCheckedModeBanner: false,
+          home: SplashScreen()),
+    );
   }
 }
+
