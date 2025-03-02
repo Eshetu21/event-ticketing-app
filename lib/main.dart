36974984790cda40d17f6714f70485582cf78bc0
Eshetu21 <eshetutesema2050@gmail.com>
2025-03-02 18:18:55 +0300

    added DI

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,4 @@
 import 'package:crypto/core/theme/theme.dart';
-import 'package:crypto/features/auth/data/datasources/auth_remote_data_source.dart';
-import 'package:crypto/features/auth/data/repositories/auth_repository_imp.dart';
-import 'package:crypto/features/auth/domain/usecases/login_usecase.dart';
-import 'package:crypto/features/auth/domain/usecases/register_usercase.dart';
 import 'package:crypto/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:crypto/injection_container.dart';
 import 'package:crypto/pages/splash_screen.dart';
